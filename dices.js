7405ad78c3183023d41174d2598616782d544c2f
Matheus Ribeiro <matheus.tex02@gmail.com>
2022-06-30 02:00:41 -0300

    d6 and d4 functional

--- dices.js
+++ dices.js
@@ -1,9 +1,10 @@
 let contentBox = document.getElementById("content-box")
-let counter = document.getElementById("counter")
 let sumResult = document.getElementById("sum")
-let sumDice = 0 
+let sumD6 = 0 
+let sumD4 = 0
 let sum = Array()
 let i = 0
+sum = []
 window.onload = Clear()
     function Rolld20(){
         let d20 = Math.floor(Math.random()* 20) + 1
@@ -21,24 +22,40 @@ window.onload = Clear()
         contentBox.innerHTML = 'd8 = ' + d8
     }
     function Rolld6(){
-        sumDice = sumDice + 1
-        counter.innerHTML =  sumDice
-        document.getElementById("counter").style.display="inline"    
+        sumD6 = sumD6 + 1
+        document.getElementById("counter-d6").innerHTML =  sumD6
+        document.getElementById("border-d6").style.display="inline"
+        document.getElementById("counter-d6").style.display="inline"
     }
 
     function Rolld4(){
-        let d4 = Math.floor(Math.random()* 4) + 1
-        console.log(d4)
-        contentBox.innerHTML = 'd4 = ' + d4
+        sumD4 = sumD4 + 1
+        console.log('soma d4 = ' + sumD4)
+        document.getElementById("counter-d4").innerHTML =  sumD4    
+        document.getElementById("counter-d4").style.display="inline"
+        document.getElementById("border-d4").style.display="inline"
+        
+        
     }
 
     function Roll(){
-        i = 0
         sum = []
-        while (i < sumDice){
-            d6 = (Math.floor(Math.random()* 6) + 1)
-            sum.push(d6)
-            i++
+        i= 0
+        if(sumD6 != 0){ //se tiver d6 faça
+            while (i < sumD6){ //enquanto i menor que totalD6
+                d6 = (Math.floor(Math.random()* 6) + 1)
+                sum.push(d6)
+                i++
+            }
+        }
+
+        i=0
+        if(sumD4 != 0){
+            while (i < sumD4){
+                d4 = (Math.floor(Math.random()* 4) + 1)
+                sum.push(d4)
+                i++
+            }
         }
 
         sumResult.innerHTML = ''
@@ -49,9 +66,7 @@ window.onload = Clear()
             
                 sumResult.innerHTML += ' + ' + sum[i] 
                 
-            console.log('legth = ' + sum.length)
-            console.log('i = '+ i)
-            console.log(sum[i])
+           
             res += sum[i];
         }
        
@@ -59,8 +74,12 @@ window.onload = Clear()
         contentBox.innerHTML = res
     }
     function Clear(){
-        sumDice =0
-        document.getElementById("counter").style.display="none"
+        sumD6 = 0
+        sumResult.innerHTM = 'null'
+        document.getElementById("border-d6").style.display="none"
+        document.getElementById("counter-d6").style.display="none"
+        document.getElementById("border-d4").style.display="none"
+        document.getElementById("counter-d4").style.display="none"
     }
    
     
