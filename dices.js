013d8672d1527ed2904d3c0d1fd0625e94074d1c
Matheus Ribeiro <matheus.tex02@gmail.com>
2022-06-30 22:22:33 -0300

    clean code refactoring

--- dices.js
+++ dices.js
@@ -1,51 +1,87 @@
 let contentBox = document.getElementById("content-box")
 let sumResult = document.getElementById("sum")
 let cal = document.getElementById("calculation")
-let sumD6 = 0 
-let sumD4 = 0
-let sum = Array()
-let i = 0
-sum = []
-let calculation = 0
-window.onload = Clear()
-    function Rolld20(){
-        let d20 = Math.floor(Math.random()* 20) + 1
-        console.log(d20)
-        contentBox.innerHTML = 'd20 = ' + d20
-    }
-    function Rolld12(){
-        let d12 = Math.floor(Math.random()* 12) + 1
-        console.log(d12)
-        contentBox.innerHTML = 'd12 = ' + d12
-    }
-    function Rolld8(){
-        let d8 = Math.floor(Math.random()* 8) + 1
-        console.log(d8)
-        contentBox.innerHTML = 'd8 = ' + d8
-    }
-    function Rolld6(){
-        sumD6 = sumD6 + 1
-        document.getElementById("counter-d6").innerHTML =  sumD6
-        document.getElementById("border-d6").style.display="inline"
-        document.getElementById("counter-d6").style.display="inline"
-    }
+let diceAmount = []
+let diceArray = [
+    2,
+    4,
+    6,
+    8,
+    10,
+    12,
+    20,
+    100
+]
 
-    function Rolld4(){
-        sumD4 = sumD4 + 1
-        console.log('soma d4 = ' + sumD4)
-        document.getElementById("counter-d4").innerHTML =  sumD4    
-        document.getElementById("counter-d4").style.display="inline"
-        document.getElementById("border-d4").style.display="inline"
-        
-        
+//--------------
+
+
+function sortResult(diceType){
+    return Math.floor(Math.random()* diceType) + 1
+}
+
+function generateDices(diceArray){
+    let dicesComponent = ""
+    for (let index = 0; index < diceArray.length; index++) {
+
+        dicesComponent +=
+        `<div class="border d${diceArray[index]}" id="border-d${diceArray[index]}">
+            <p class="counter d${diceArray[index]}" id="counter-d${diceArray[index]}"></p>
+        </div>
+        <button class="roll-button d${diceArray[index]}" onclick="showDicesResult(${diceArray[index]})">
+            <img src="images/d.png" alt="d${diceArray[index]}">
+        </button>`
     }
+    return dicesComponent
+}
+
+function showDices(htmlComponent){
+    var dicesBarComponent = document.getElementById("dices-bar")
+    dicesBarComponent.innerHTML = htmlComponent
+}
+
+function rollDice(dice, diceQnt){
+    var dicesSum = 0
+    var value
+    diceAmount = []
+    for (let index = 0; index < diceQnt; index++) {
+        value = sortResult(dice)
+        diceAmount.push(value)
+        dicesSum += value
+    }
+    console.log(dicesSum)
+    return dicesSum
+
+}
+
+
+function showDicesResult(diceResult){
+    var value = rollDice(diceResult,1)
+    
+    putResultOnScreen(value)
+    showAllDices()
+}
+function showAllDices(){
+    sumResult.innerHTML = ''
+    sumResult.innerHTML += diceAmount.sort(function(a, b){return a - b})
+}
+function putResultOnScreen(diceResult){
+    var contentBox = document.getElementById("content-box")
+    contentBox.innerHTML = diceResult
+}
+
+showDices(generateDices(diceArray))
+
+
+//---------------------------------
     function Acrescing(){
-        if(sumD4 != 0 && sumD6 != 0){
-            calculation += '+'
+        if(diceAmount > 1){
+            
         }    
     }
 
     function Rolling(){
+        i=0
         if(sumD6 != 0){ //se tiver d6 faça
             while (i < sumD6){ //enquanto i menor que totalD6
                 d6 = (Math.floor(Math.random()* 6) + 1)
@@ -67,41 +103,29 @@ window.onload = Clear()
             }
             calculation += i + 'd4 '
         }
-    }
-
-
-    function Roll(){
-        calculation = ''
-        sum = []
-        Rolling()
-        cal.innerHTML = calculation
-        
-        sumResult.innerHTML = ''
-        sumResult.innerHTML += sum.sort(function(a, b){return a - b})
-         
-        let res = 0
-        for (let i = 0; i < sum.length; i++){
-            res += sum[i];
+        i=0
+        if(sumD2 != 0){
+            Acrescing()
+            while (i < sumD2){
+                
+                d2 = (Math.floor(Math.random()* 2) + 1)
+                sum.push(d2)
+                i++
+            }
+            calculation += i + 'd2 '
         }
-        contentBox.innerHTML = res
     }
-
-
 
     function Clear(){
         sumD6 = 0
         sumD4 = 0
-        
+        sumD2 = 0
         sumResult.innerHTM = 'null'
-        document.getElementById("calculation").innerHTML = ''
+        
         document.getElementById("border-d6").style.display="none"
         document.getElementById("counter-d6").style.display="none"
         document.getElementById("border-d4").style.display="none"
         document.getElementById("counter-d4").style.display="none"
+        document.getElementById("border-d2").style.display="none"
+        document.getElementById("counter-d2").style.display="none"
     }
-   
-    
-        
-    
-  
-    
